c96db6478a6b3a4cc1b801956a58ae85b2581095
Abys5 <tomburridge2002@gmail.com>
2020-06-20 23:20:10 +0100

    Init Structure

---
+++ cmake-build-debug/CMakeFiles/Move_it.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Move_it.dir/main.cpp.obj"
+  "Move_it.exe"
+  "Move_it.exe.manifest"
+  "Move_it.pdb"
+  "libMove_it.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Move_it.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
